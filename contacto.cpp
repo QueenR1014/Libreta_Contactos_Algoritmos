3f2d47388268c949f11517dbe90733bc5430aed6
QueenR1014 <129343853+QueenR1014@users.noreply.github.com>
2023-11-06 22:07:17 -0500

    Add files via upload

---
+++ contacto.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+map<string,string> crear_redes(){
+    map<string, string> redes;
+    redes["Instagram"] = "";
+    redes["Correo"] = "";
+    redes["X"] = "";
+    redes["LinkedIn"] = "";
+
+    return redes;
+}
+
+class contacto{
+    private:
+        string nombre;
+        int telefono;
+        map<string, string> redes = crear_redes();
+        string direccion;
+        
+    public:
+
+        contacto(){
+            
+        }
+
+        void set_nombre(string n ){
+
+        }
+};
