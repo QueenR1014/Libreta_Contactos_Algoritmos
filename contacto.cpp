7f51da9f4184a229b897738b88db9821ec016a4f
QueenR1014 <129343853+QueenR1014@users.noreply.github.com>
2023-11-11 21:02:06 -0500

    Add files via upload

--- contacto.cpp
+++ contacto.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <map>
+#include <string>
 using namespace std;
 
 map<string,string> crear_redes(){
@@ -25,7 +26,51 @@ class contacto{
             //test
         }
 
-        void set_nombre(string n ){
-
+        void set_nombre(string n){
+            nombre = n;
         }
+
+        string get_nombre(){
+            return nombre;
+        }
+
+        void set_telf(int t){
+            telefono  = t;
+        }
+
+        int get_telf(){
+            return telefono;
+        }
+
+        void set_red(string red , string info){
+            redes[red] = info;
+        }
+
+        string get_red(string red){
+            return redes[red];
+        }
+
+        void set_direcc(string d){
+            direccion = d;
+        }
+
+        string get_direcc(){
+            return direccion;
+        }
+        string print_map(){
+
+            string redes_string;
+            for(const auto& par : redes){
+                redes_string += par.first + ": " + par.second + " \n";
+            }
+
+            return redes_string;
+        }
+
+        string print(){
+            return "Nombre: " + nombre + " Teléfono: " + to_string(telefono) +  " Dirección: " + direccion;
+        }
+
+
+        
 };
