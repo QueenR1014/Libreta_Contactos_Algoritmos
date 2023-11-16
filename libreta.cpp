17b79ca47520c8c725d632d78549039bd309225a
tumbalocas3000 <150102094+tumbalocas3000@users.noreply.github.com>
2023-11-15 21:59:39 -0500

    libreta.cpp

--- libreta.cpp
+++ libreta.cpp
@@ -1,28 +1,99 @@
-#include <vector>
-#include "contacto.cpp"
-class libreta{
-private:
-    vector<contacto> contactos;
-public:
-    libreta(){
 
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////
+
+
+
+
+class LibretaContactos {
+private:
+    vector<Contacto> contactos;
+
+public:
+
+///////////////
+    void agregarContacto(const Contacto& nuevoContacto) {
+        contactos.push_back(nuevoContacto);
+        
+        
+        //////
+        
+        sort(contactos.begin(), contactos.end(), [](const Contacto& a, const Contacto& b) {
+            return a.get_nombre() < b.get_nombre();
+        });
     }
-    void insert_c(contacto c){
-        contactos.push_back(c);
+
+
+////////////////////
+    void eliminarContacto(const string& nombre) {
+        auto it = remove_if(contactos.begin(), contactos.end(),
+                            [nombre](const Contacto& contacto) { return contacto.get_nombre() == nombre; });
+
+        contactos.erase(it, contactos.end());
     }
-    contacto buscar_cont(string nom){
-        for(int i = 0; i < contactos.size(); i++){
-            if(nom == contactos[i].get_nombre()) {
-                return contactos[i];
+
+
+//////////////////
+    void buscarContacto(const string& nombre) const {
+        auto it = find_if(contactos.begin(), contactos.end(),
+                          [nombre](const Contacto& contacto) { return contacto.get_nombre() == nombre; });
+
+        if (it != contactos.end()) {
+            cout << *it << endl;
+            cout << "Redes:\n" << it->print_map() << endl;
+        } else {
+            cout << "Contacto no encontrado." << endl;
+        }
+    }
+
+
+
+////////////////////////
+    void mostrarContactos() const {
+        for (const auto& contacto : contactos) {
+            cout << contacto << endl;
+            cout << "Redes:\n" << contacto.print_map() << endl;
+        }
+    }
+
+
+
+//////////////////////////
+    void mostrarCantidadContactos() const {
+        cout << "Total de contactos: " << contactos.size() << endl;
+    }
+
+    void mostrarContactosPorLetra(char letra) const {
+        for (const auto& contacto : contactos) {
+            if (tolower(contacto.get_nombre()[0]) == tolower(letra)) {
+                cout << contacto << endl;
+                cout << "Redes:\n" << contacto.print_map() << endl;
             }
         }
-        cout<<"contaco no encontrado :("<<endl;
-        return contacto();
     }
 
+
+
+////////////////////////////////////////
+    void realizarCopiaSeguridad(const string& nombreArchivo) const {
+        ofstream archivo(nombreArchivo);
+
+        for (const auto& contacto : contactos) {
+            archivo << contacto << endl;
+            archivo << "Redes:\n" << contacto.print_map() << endl;
+        }
+
+        archivo.close();
+    }
 };
 
 
-int main(){
 
+
+int main() {
+
+    return 0;
 }
