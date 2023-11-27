0075b1b05e55aa805ec0af603fb6aa51323caf4f
DanielM13031 <54962223+DanielM13031@users.noreply.github.com>
2023-11-27 17:49:44 -0500

    Update libreta.cpp

--- libreta.cpp
+++ libreta.cpp
@@ -16,32 +16,6 @@ private:
     vector<Contacto> contactos;
 
 public:
-
-
-    /*vector<Contacto> insercionOrdenada(Contacto nuevo){
-
-        //Vector displacement acording to name
-        bool inserted = false;
-        vector<Contacto> temp;
-        for(int i = 0; i < contactos.size(); i++ ){
-            if(contactos[i] < nuevo && !inserted){
-                temp[i] = contactos[i];
-                temp[i + 1] = nuevo; 
-            }else if(!inserted){
-                temp[i] = nuevo;
-            }else{
-                temp[i] = contactos[i];
-            }
-        }
-        
-        contactos = temp;
-        return temp;
-    }*/
-
-
-
-///////////////
-
     void AgregarContacto(const Contacto& nuevoContacto) {
         contactos.push_back(nuevoContacto);
 
@@ -51,7 +25,6 @@ public:
     }
 
 
-////////////////////
 
     void EliminarContacto(const string& nombre) {
         auto it = remove_if(contactos.begin(), contactos.end(), [nombre](Contacto& contacto) { return contacto.get_nombre() == nombre; });
@@ -62,7 +35,6 @@ public:
     }
 
 
-//////////////////
 
     void BuscarContacto(const string& nombre) {
         auto it = find_if(contactos.begin(), contactos.end(),
@@ -79,10 +51,6 @@ public:
         }
     }
 
-
-
-////////////////////////
-
     void MostrarContactos() const {
         for (Contacto c : contactos) { //For en el que imprime contacto por contacto
             cout<<c<< endl;
@@ -91,16 +59,10 @@ public:
     }
 
 
-
-//////////////////////////
-
     void MostrarCantidadContactos() const {
         cout << "Total de contactos: " << contactos.size() << endl;
     }
 
-
-
-/////////////////////////
 
     void MostrarContactosPorLetra(char letra) {
         for (auto& contacto : contactos) {
@@ -112,21 +74,31 @@ public:
     }
 
 
+    void RealizarCopiaSeguridad(const string& nombreArchivo, queue<string>& c_s) {
+    ofstream archivo(nombreArchivo); // Crear archivo de copia de seguridad
 
-////////////////////////////////////////
-     void RealizarCopiaSeguridad(const string& nombreArchivo){
-        ofstream archivo(nombreArchivo);                             //usando file stream, se crea la copia de seguridad como archivo .txt
-
-        for (auto& contacto : contactos) {
-            archivo << contacto << endl;
-            archivo << "Redes:\n" << contacto.print_map() << endl;    // donde se imprimen todos los contactos existentes
-        }
-
-        archivo.close();           // se cierra el archivo
+    // Escribir los contactos en el archivo
+    for (auto& contacto : contactos) {
+        archivo << contacto << endl;
+        archivo << "Redes:\n" << contacto.print_map() << endl; 
     }
+    archivo.close(); // Cerrar el archivo
+
+    c_s.push(nombreArchivo); // Agregar el nombre del archivo a la cola
+
+    int x = 0;
+    cout << "Desea ver las copias de seguridad? (1) para sí, (2) para no: ";
+    cin >> x;
+    if (x == 1) {
+        queue<string> temp = c_s; // Crear una copia temporal de la cola para no alterar la original
+        while (!temp.empty()) {
+            cout << "Archivo: " << temp.front() << endl;
+            temp.pop();
+        }
+    }
+}
 
 
-///////////////////////////////
 
     void OrdenarContactos(){
         sort(contactos.begin(), contactos.end(), []( Contacto& a,  Contacto& b) {
@@ -135,13 +107,12 @@ public:
     }
 
 
-//////////////////////////////////////////////////
     void ImportarContactos(vector<Contacto> importado){
         contactos = importado;
         OrdenarContactos();
     }
 
-/////////////////////////////////////////////////////
+
 
     vector<Contacto> RevisarFrecuencia(){
         vector<Contacto> MasFrecuentes;
@@ -168,10 +139,6 @@ public:
         return MasFrecuentes;
     }
 
-
-
-
-////////////////////////////////////////////////////
     Contacto BusquedaBinaria(string nombre){
         bool found = false;
         int max = contactos.size() - 1;
@@ -191,4 +158,3 @@ public:
     }
 
 };
-
