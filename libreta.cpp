0ee2ad42100b018744f913b757a6541843a66c5d
DanielM13031 <54962223+DanielM13031@users.noreply.github.com>
2023-11-27 23:44:18 -0500

    Update libreta.cpp

--- libreta.cpp
+++ libreta.cpp
@@ -16,42 +16,16 @@ private:
     vector<Contacto> contactos;
 
 public:
-
-
-    /*vector<Contacto> insercionOrdenada(Contacto nuevo){
-
-        //Vector displacement acording to name
-        bool inserted = false;
-        vector<Contacto> temp;
-        for(int i = 0; i < contactos.size(); i++ ){
-            if(contactos[i] < nuevo && !inserted){
-                temp[i] = contactos[i];
-                temp[i + 1] = nuevo; 
-            }else if(!inserted){
-                temp[i] = nuevo;
-            }else{
-                temp[i] = contactos[i];
-            }
-        }
-        
-        contactos = temp;
-        return temp;
-    }*/
-
-
-
-///////////////
-
     void AgregarContacto(const Contacto& nuevoContacto) {
         contactos.push_back(nuevoContacto);
+        cout << "Tamaño del vector después de agregar: " << contactos.size() << endl;
 
-        sort(contactos.begin(), contactos.end(), []( Contacto& a,  Contacto& b) { //Al añadir un contacto, automáticamente se ordena alfabéticamente
-            return a < b;
-        });
+        //sort(contactos.begin(), contactos.end(), []( Contacto& a,  Contacto& b) { //Al añadir un contacto, automáticamente se ordena alfabéticamente
+            //return a < b;
+        //});
     }
 
 
-////////////////////
 
     void EliminarContacto(const string& nombre) {
         auto it = remove_if(contactos.begin(), contactos.end(), [nombre](Contacto& contacto) { return contacto.get_nombre() == nombre; });
@@ -62,49 +36,41 @@ public:
     }
 
 
-//////////////////
 
     void BuscarContacto(const string& nombre) {
         auto it = find_if(contactos.begin(), contactos.end(),
                           [nombre](Contacto& contacto) { return contacto.get_nombre() == nombre; });
 
-        /* Función find if, de la misma manera que el remove, desde el rango begin hasta en, va sacando los nombre de cada contacto y los iguala al 
+        /* Función find if, de la misma manera que el remove, desde el rango begin hasta en, va sacando los nombre de cada contacto y los iguala al
         nombre dado */
 
         if (it != contactos.end()) {
-            cout << *it << endl; 
+            cout << *it << endl;
             cout << "Redes:\n" << it->print_map() << endl;  // si lo encuentra, se imprimen sus redes
         } else {
             cout << "Contacto no encontrado." << endl; // caso en el que no
         }
     }
 
-
-
-////////////////////////
-
     void MostrarContactos() const {
-        for (Contacto c : contactos) { //For en el que imprime contacto por contacto
-            cout<<c<< endl;
-            cout<< "Redes:\n" << c.print_map() << endl;
+        for (const Contacto& c : contactos) { // Utilizar una referencia constante para evitar problemas de tipos
+            cout << c << endl;
+            cout << "Redes:\n" << c.print_map() << endl;
         }
     }
 
 
 
-//////////////////////////
 
-    void MostrarCantidadContactos() const {
-        cout << "Total de contactos: " << contactos.size() << endl;
+    int MostrarCantidadContactos() const {
+
+        return contactos.size();
     }
 
 
-
-/////////////////////////
-
     void MostrarContactosPorLetra(char letra) {
         for (auto& contacto : contactos) {
-            if (tolower(contacto.get_nombre()[0]) == tolower(letra)) {  // dentro de contactos, se tomas todos los contactos cuya primera letra (convertida a 
+            if (tolower(contacto.get_nombre()[0]) == tolower(letra)) {  // dentro de contactos, se tomas todos los contactos cuya primera letra (convertida a
                 cout << contacto << endl;                               // minúscula) sean igual a la letra dada y se imprimen.
                 cout << "Redes:\n" << contacto.print_map() << endl;
             }
@@ -112,21 +78,31 @@ public:
     }
 
 
+    void RealizarCopiaSeguridad(const string& nombreArchivo, queue<string>& c_s) {
+    ofstream archivo(nombreArchivo); // Crear archivo de copia de seguridad
 
-////////////////////////////////////////
-     void RealizarCopiaSeguridad(const string& nombreArchivo){
-        ofstream archivo(nombreArchivo);                             //usando file stream, se crea la copia de seguridad como archivo .txt
-
-        for (auto& contacto : contactos) {
-            archivo << contacto << endl;
-            archivo << "Redes:\n" << contacto.print_map() << endl;    // donde se imprimen todos los contactos existentes
-        }
-
-        archivo.close();           // se cierra el archivo
+    // Escribir los contactos en el archivo
+    for (auto& contacto : contactos) {
+        archivo << contacto << endl;
+        archivo << "Redes:\n" << contacto.print_map() << endl;
     }
+    archivo.close(); // Cerrar el archivo
+
+    c_s.push(nombreArchivo); // Agregar el nombre del archivo a la cola
+
+    int x = 0;
+    cout << "Desea ver las copias de seguridad? (1) para sí, (2) para no: ";
+    cin >> x;
+    if (x == 1) {
+        queue<string> temp = c_s; // Crear una copia temporal de la cola para no alterar la original
+        while (!temp.empty()) {
+            cout << "Archivo: " << temp.front() << endl;
+            temp.pop();
+        }
+    }
+}
 
 
-///////////////////////////////
 
     void OrdenarContactos(){
         sort(contactos.begin(), contactos.end(), []( Contacto& a,  Contacto& b) {
@@ -135,13 +111,12 @@ public:
     }
 
 
-//////////////////////////////////////////////////
     void ImportarContactos(vector<Contacto> importado){
         contactos = importado;
         OrdenarContactos();
     }
 
-/////////////////////////////////////////////////////
+
 
     vector<Contacto> RevisarFrecuencia(){
         vector<Contacto> MasFrecuentes;
@@ -168,10 +143,6 @@ public:
         return MasFrecuentes;
     }
 
-
-
-
-////////////////////////////////////////////////////
     Contacto BusquedaBinaria(string nombre){
         bool found = false;
         int max = contactos.size() - 1;
@@ -191,4 +162,3 @@ public:
     }
 
 };
-
