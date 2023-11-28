fd1002235d7c2b2eb8442dfc212bf6fc8ac930df
DanielM13031 <54962223+DanielM13031@users.noreply.github.com>
2023-11-27 23:43:03 -0500

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,10 +1,20 @@
 int main() {
     calltext("nombre.txt");
-    
-    cout<<"###################################### \n ¡Bienvenid@! Que deseas hacer hoy: "<<endl;
-    cout<<" (1) Ver Contactos \n (2) Editar contactos "<<endl;
+    LibretaContactos libreta;
+
+    cout << "###################################### \n ¡Bienvenid@! ¿Qué deseas hacer hoy?" << endl;
+    cout << " (1) Ver Contactos \n (2) Editar contactos " << endl;
+
     int option0;
-    cin>>option0;
-    menu(option0);
+    cin >> option0;
+    menu(option0, libreta);
+
+    while (option0 != 0) {
+        cout << "###################################### \n ¿Qué deseas hacer ahora? (0 para salir): " << endl;
+        cout << "(1) Ver Contactos " << endl << "(2) Editar contactos " << endl;
+
+        cin >> option0;
+        menu(option0, libreta); // Se pasa la instancia de libreta a la función menu
+    }
     return 0;
 }
