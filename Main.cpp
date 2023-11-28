b5e3cb4071a241a355997aea0952086f577499cc
DanielM13031 <54962223+DanielM13031@users.noreply.github.com>
2023-11-27 19:45:24 -0500

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,10 @@
+int main() {
+    calltext("nombre.txt");
+    
+    cout<<"###################################### \n ¡Bienvenid@! Que deseas hacer hoy: "<<endl;
+    cout<<" (1) Ver Contactos \n (2) Editar contactos "<<endl;
+    int option0;
+    cin>>option0;
+    menu(option0);
+    return 0;
+}
